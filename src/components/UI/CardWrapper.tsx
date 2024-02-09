5a44c5970e14ce9b82b5a36604c70d5f24fa07ad
robinaasan <robin.aasan@hotmail.com>
2024-02-09 12:50:44 +0100

    fixed card alignment

--- src/components/UI/CardWrapper.tsx
+++ src/components/UI/CardWrapper.tsx
@@ -1,4 +1,4 @@
-import { Card, Divider, Grid, styled } from '@mui/material';
+import { Box, Card, Divider, Grid, styled } from '@mui/material';
 
 interface Props {
     firstChild: JSX.Element;
@@ -18,21 +18,27 @@ const CardWrapper = ({ firstChild, secondChild, middleChild }: Props) => {
     return (
         <StyledCard>
             <Grid container direction={'row'} wrap="nowrap" minHeight={'4rem'}>
-                <Grid justifyContent={'space-between'} item xs={middleChild ? 4 : 6} p={3}>
-                    {firstChild}
+                <Grid item xs={middleChild ? 4 : 6} p={3}>
+                    <Box width={'90%'} margin={'auto'}>
+                        {firstChild}
+                    </Box>
                 </Grid>
                 {middleChild && (
                     <>
                         <Divider orientation="vertical" flexItem sx={{ my: 0.7 }} />
                         <Grid item xs={4} p={3}>
-                            {middleChild}
+                            <Box width={'90%'} margin={'auto'}>
+                                {middleChild}
+                            </Box>
                         </Grid>
                     </>
                 )}
 
                 <Divider orientation="vertical" flexItem sx={{ my: 0.7 }}></Divider>
                 <Grid item xs={middleChild ? 4 : 6} p={3}>
-                    {secondChild}
+                    <Box width={'90%'} margin={'auto'}>
+                        {secondChild}
+                    </Box>
                 </Grid>
             </Grid>
         </StyledCard>
