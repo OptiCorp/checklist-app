ab5063fc4c5e672baecd9c8c773c7ee6baed46f7
Malin Michelle Riisem Edvardsen <malinmichelle@hotmail.com>
2024-02-13 11:33:08 +0100

    mobdemob page with lists for cards

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,93 @@
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
+import { ListItemButton, Collapse, List, ListItemIcon, Box, Stack, Grid } from '@mui/material';
+import CardWrapper from './CardWrapper';
+import React from 'react';
+import { listTextType } from './NestedList';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+    item: listTextType;
+    index: number;
+    openState: boolean;
+    handleClick: (index: number) => void;
+}
+
+const Card: React.FC<Props> = ({ item, index, openState, handleClick }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Box sx={{ mt: 5 }}>
+            <Stack spacing={{ xs: 1.5, sm: 2, md: 4, lg: 4 }}>
+                <ListItemButton onClick={() => handleClick(index)} sx={{ mt: 5 }}>
+                    {openState ? <ExpandLess /> : <ExpandMore />}
+                    <CardWrapper
+                        firstChild={
+                            <Grid
+                                component={'li'}
+                                container
+                                wrap="nowrap"
+                                justifyContent={'space-between'}
+                            >
+                                <Grid item sx={{ textWrap: 'nowrap' }}>
+                                    <b>awdaw:</b>
+                                </Grid>
+                                <Grid item>asd alksdnlka</Grid>
+                            </Grid>
+                        }
+                        secondChild={
+                            <Grid
+                                component={'li'}
+                                container
+                                wrap="nowrap"
+                                justifyContent={'space-between'}
+                            >
+                                <Grid item sx={{ textWrap: 'nowrap' }}>
+                                    <b>awdaw:</b>
+                                </Grid>
+                                <Grid item>asd alksdnlka</Grid>
+                            </Grid>
+                        }
+                    />
+                </ListItemButton>
+                <Collapse in={openState || false} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding sx={{ pl: 12 }}>
+                        <ListItemButton>
+                            <ListItemIcon sx={{ width: '100%' }} onClick={() => navigate('/')}>
+                                <CardWrapper
+                                    firstChild={
+                                        <Grid
+                                            component={'li'}
+                                            container
+                                            wrap="nowrap"
+                                            justifyContent={'space-between'}
+                                        >
+                                            <Grid item sx={{ textWrap: 'nowrap' }}>
+                                                <b>{item.text}</b>
+                                            </Grid>
+                                            <Grid item>asd alksdnlka</Grid>
+                                        </Grid>
+                                    }
+                                    secondChild={
+                                        <Grid
+                                            component={'li'}
+                                            container
+                                            wrap="nowrap"
+                                            justifyContent={'space-between'}
+                                        >
+                                            <Grid item sx={{ textWrap: 'nowrap' }}>
+                                                <b>awdaw:</b>
+                                            </Grid>
+                                            <Grid item>asd alksdnlka</Grid>
+                                        </Grid>
+                                    }
+                                />
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </List>
+                </Collapse>
+            </Stack>
+        </Box>
+    );
+};
+
+export default Card;
